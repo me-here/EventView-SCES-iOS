c04ac6a108684580cb9fc0fa723e2e5b03875778
Mihir Thanekar <mihirthanekar@gmail.com>
2021-01-20 14:18:24 -0800

    initial commit

---
+++ EventView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventView
+//
+//  Created by Mihir Thanekar on 1/20/21.
+//  Copyright © 2021 Mihir Thanekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
