4d0f8d2b30794347718d6b5f53e3f3971a1e48ad
Mihir Thanekar <mihirthanekar@gmail.com>
2021-01-20 15:06:10 -0800

    Created scaffold for EventAPIClient class.

---
+++ EventView/Routing/EventAPIClient.swift
@@ -0,0 +1,31 @@
+//
+//  EventAPIClient.swift
+//  EventView
+//
+//  Created by Mihir Thanekar on 1/20/21.
+//  Copyright © 2021 Mihir Thanekar. All rights reserved.
+//
+
+import Foundation
+
+class EventAPIClient {
+    func getEventList() -> [Event]? {
+        return []
+    }
+    
+    func getEventWith(eventID: String) -> Event? {
+        return nil
+    }
+    
+    func setUserIsAttendingEventWith(eventID: String) {
+        
+    }
+    
+    func attemptLogin(user: User) {
+        
+    }
+    
+    func attemptSignup(user: User) {
+        
+    }
+}
