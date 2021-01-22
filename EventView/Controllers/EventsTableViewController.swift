edd5b0effae305016676fe445a6929a560b32db3
Mihir Thanekar <mihirthanekar@gmail.com>
2021-01-21 20:05:06 -0800

    Displayed events in table view

---
+++ EventView/Controllers/EventsTableViewController.swift
@@ -0,0 +1,55 @@
+//
+//  EventsTableViewController.swift
+//  EventView
+//
+//  Created by Mihir Thanekar on 1/21/21.
+//  Copyright © 2021 Mihir Thanekar. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewController: UITableViewController {
+    
+    var eventList: [Event] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        EventAPIClient.getEventList(handle: { result in
+            switch result {
+            case .success(let events):
+                self.eventList = events
+                self.tableView.reloadData()
+            case .failure(let error):
+                print("failed with \(error)")
+            }
+        })
+    }
+
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventTableViewCell", for: indexPath)
+        print(eventList)
+        cell.textLabel?.text = eventList[indexPath.row].name
+        cell.detailTextLabel?.text = eventList[indexPath.row].location
+        return cell
+    }
+
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+}
